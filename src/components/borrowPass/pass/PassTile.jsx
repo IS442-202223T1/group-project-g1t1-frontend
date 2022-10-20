fb65c1320a5cdc651ccb88988e0f684b3829e168
theianchia <theianchia@users.noreply.github.com>
2022-10-20 12:25:34 +0800

    Fixed typo

--- src/components/borrowPass/pass/PassTile.jsx
+++ src/components/borrowPass/pass/PassTile.jsx
@@ -6,7 +6,7 @@ export default function PassTile({title, imgUrl, desc}){
       <a href="/#" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row hover:bg-gray-100">
         <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={imgUrl} alt="nature" />
         <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900e">{title}</h5>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{title}</h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
           <a href="/#" className="inline-flex items-center font-medium text-redPri hover:text-redSec">
             Learn more
