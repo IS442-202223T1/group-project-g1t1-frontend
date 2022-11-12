f06e501d513c6ef2b6e1fa006389bca1ee496d80
Rachel Yong <73299855+rachelyongies@users.noreply.github.com>
2022-11-12 20:26:06 +0800

    View Employees Page

--- src/components/employees/index.jsx
+++ src/components/employees/index.jsx
@@ -1,52 +1,66 @@
-import React from "react";
-import DefaultSubmitButton from "../common/buttons/defaultSubmitButton";
+/* eslint-disable no-plusplus */
 
-const getEmployeeDetails = () => {
-  //
-}
+import React, { useState, useEffect } from "react";
+import { getAllUser } from "../../api/account";
+
 
 function Employees() {
-  return (
-    <div className="max-w-5xl mt-5 mx-auto">
-      <h1 className="py-4 font-medium text-3xl">View Employees</h1>
 
-      <div className="overflow-x-auto relative">
-    <table className="py-3 w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    const token = sessionStorage.getItem("token");
+    const [allUsers, setAllUsers] = useState([])
+    const users = [];
+
+  useEffect(() => {
+    renderUsers();
+    async function renderUsers() {
+        const allUsers = await getAllUser(token);
+        allUsers.forEach((user) => {
+            users.push(user);
+                for (let i = 0; i < allUsers.length; i++) {
+                  if (allUsers[i].roles.label === "borrower") {
+                    users.push(allUsers[i]);
+                    }
+                }
+        })
+        setAllUsers(users);
+      }
+  },[]);
+  
+return (
+    <div className="max-w-5xl mt-5 mx-auto">
+      <h1 className="font-medium text-3xl">View Employees</h1>
+    
+<div className="p-4 bg-white rounded-lg md:p-8">
+      <div className="shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-700">
+          <thead className="text-xs text-gray-700 uppercase">
             <tr>
-                <th scope="col" className="py-3 px-6">
-                   Employee Name
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Email
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Number of Loans 
-                </th>
-                <th scope="col" className="py-3 px-6">
-                View Details
-                </th>
+              <th scope="col" className="py-3 px-6 bg-gray-50">
+                Employee Name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Email
+              </th>
+              
             </tr>
-        </thead>
-        <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    getName
-                </th>
-                <td className="py-4 px-6">
-                    getEmail
-                </td>
-                <td className="py-4 px-6">
-                    getNumberOfLoans
-                </td>
-                <td className="py-4 px-6">
-                <DefaultSubmitButton buttonName="View Details" getEmployeeDetails={getEmployeeDetails} />
-                </td>
-                
-            </tr>
-        </tbody>
-    </table>
-</div>
+          </thead>
+          <tbody>
+            {
+              allUsers.map((user) => 
+                <tr className="bg-white divide-y">
+                  <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-100">
+                    {user.name}
+                  </th>
+                  <td className="py-4 px-6">
+                    {user.email}
+                  </td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
     </div>
   )
 }
