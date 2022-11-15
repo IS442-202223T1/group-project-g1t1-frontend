85f2bd6d60c7b1b976c3c4c9adc264f9cd342c8b
theianchia <theianchia@users.noreply.github.com>
2022-11-15 22:41:09 +0800

    Updated UI for searchbar and reports

--- src/components/reports/index.jsx
+++ src/components/reports/index.jsx
@@ -5,8 +5,6 @@ import AnnualReport from "./annualReport";
 import BiannualReport from "./biannualReport";
 import ButtonTab from "./buttonTab";
 
-
-
 function Reports() {
   const buttons = ["Employee Monthly Reports", "Employee Bi-Annual Reports", "Employee Annual Reports", "Monthly Report"];
   const [activeTabIndex, setActiveTabIndex] = useState(0);
@@ -24,12 +22,11 @@ function Reports() {
         activeTabIndex={activeTabIndex}
       />
     </li>
-  ))
-  return (
+  ));
 
+  return (
     <div className="max-w-5xl mt-5 mx-auto">
       <h1 className="font-medium text-3xl">View Reports</h1>
-
       <div className="max-w-5xl mt-5 mx-auto">
       <div className="w-full bg-white rounded-lg border shadow-md">
       <ul className="flex flex-wrap items-center justify-between text-sm font-medium text-center bg-gray-50 rounded-t-lg border-b border-gray-200">
@@ -38,7 +35,6 @@ function Reports() {
         </div>
       </ul>
       </div>
-      
     </div>
       {activeTabIndex === 0 ? <EmployeeReport /> : null}
       {activeTabIndex === 1 ? <BiannualReport /> : null}
