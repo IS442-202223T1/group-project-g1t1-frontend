c93823ff0efa1f096fb43994a81bcb8efd6f72ed
Rachel Yong <73299855+rachelyongies@users.noreply.github.com>
2022-11-16 01:45:27 +0800

    Add status badge for history and upcoming

--- src/components/pastBookings/pass/PassTile.jsx
+++ src/components/pastBookings/pass/PassTile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PassStatusBadge from "src/components/updateMembershipDetails/updateMembershipDetailsBody/statusBadge";
 
-export default function PassTile({title, imageUrl, date, ppl, passId, status, bookingID, fee, prevBookerDate, prevBookerName, prevBookerNum}){
+export default function PassTile({title, imageUrl, date, ppl, number, desc, passId, status, bookingID, fee, prevBookerDate, prevBookerName, prevBookerNum}){
   return (
     <div className='mb-5 grid-col-span-full md:col-span-1 rounded-lg flex border hover:shadow-lg'>
       <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={imageUrl} alt="nature" />
@@ -12,6 +12,7 @@ export default function PassTile({title, imageUrl, date, ppl, passId, status, bo
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Date: {date}</p>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Max Number of Accompanying Guests: {ppl}</p>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Status: <PassStatusBadge status={status} /></p>
+
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{fee=== 0 ? "" : "Fees Owed: " + fee}</p>
       </div>
     </div>
